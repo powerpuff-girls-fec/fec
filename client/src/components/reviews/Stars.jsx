28eec546eac37175a9260022b66dc0daf162673c
Zynh Ludwig <Zynh0722@gmail.com>
2022-03-31 16:16:16 -0700

    Finish stars component

--- client/src/components/reviews/Stars.jsx
+++ client/src/components/reviews/Stars.jsx
@@ -4,36 +4,53 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const StarContainer = styled.div`
-  position: relative;
   font-size: 2rem;
+  display: flex;
+  width: 4.5em;
 `;
 
-const EmptyStars = styled.div`
-  position: absolute;
+const EmptyStar = styled.div`
+  position: relative;
   line-height: 1em;
-  display: inline-block;
-  width: fit-content;
-  height: fit-content;
 `;
 
-const FullStars = styled(EmptyStars)`
+const FullStar = styled(EmptyStar)`
   position: relative;
   top: -1em;
-  width: ${({ stars }) => (Math.round(stars / 5 / 0.05) * 0.05) * 100}%;
+  width: ${({ fill }) => Math.round((fill * 100) / 25) * 25}%;
   overflow: hidden;
 `;
 
+function Star({ fill }) {
+  return (
+    <div>
+      <EmptyStar>☆</EmptyStar>
+      <FullStar fill={fill}>★</FullStar>
+    </div>
+  );
+}
+
 export default function Stars({ stars }) {
+  const starsArray = Array(5).fill(0);
+  let starsRemaining = stars;
+  for (let i = 0; starsRemaining > 0; i += 1) {
+    starsArray[i] = Math.min(starsRemaining, 1);
+    starsRemaining -= 1;
+  }
+
+  console.log(starsArray);
+
   return (
     <StarContainer>
-      <EmptyStars>
-        ☆☆☆☆☆
-        <FullStars stars={stars}>★★★★★</FullStars>
-      </EmptyStars>
+      {starsArray.map((fill) => <Star fill={fill} />)}
     </StarContainer>
   );
 }
 
 Stars.propTypes = {
   stars: PropTypes.number.isRequired,
+};
+
+Star.propTypes = {
+  fill: PropTypes.number.isRequired,
 };
