cb5bc9f6f7bb62ddb434ab2a23d92e82f5f334c6
Zynh Ludwig <Zynh0722@gmail.com>
2022-03-31 15:15:06 -0700

    Add reviews container and include rating breakdown

--- client/src/components/reviews/Reviews.jsx
+++ client/src/components/reviews/Reviews.jsx
@@ -5,10 +5,11 @@ import styled from 'styled-components';
 import RatingBreakdown from './rating-breakdown/RatingBreakdown';
 
 const Container = styled.div`
-  width: 100%;
+  width: 75%;
   height: 10em;
   display: grid;
   grid-template-columns: 1fr 3fr;
+  margin: auto;
 `;
 
 export default function Reviews() {
