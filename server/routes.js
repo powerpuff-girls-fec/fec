76c68243be3ca4660bc0dad33a195a0de0922f92
Zynh Ludwig <Zynh0722@gmail.com>
2022-04-08 15:00:02 -0700

    tech-debt: making catches consistent and removing logs

--- server/routes.js
+++ server/routes.js
@@ -15,39 +15,31 @@ module.exports.getReviews = (req, res) => {
 // GET /api/questions
 module.exports.getQuestions = (req, res) => {
   handlers.getQuestions(65631)
-    .then((result) => res.send(result.data));
+    .then((result) => res.send(result.data))
+    .catch((err) => res.send(`Error: ${err.message}`));
 };
 
 // GET /api/reviews/meta/:product_id
 module.exports.getReviewsMeta = (req, res) => {
   handlers.getReviewsMeta(req.params.product_id)
-    .then((result) => {
-      // console.log(result.data);
-      res.send(result.data);
-    })
-    .catch((err) => console.log(err.message));
+    .then((result) => res.send(result.data))
+    .catch((err) => console.log(`Error: ${err.message}`));
 };
 
 module.exports.putReviewHelpful = (req, res) => {
   handlers.markReviewHelpful(req.params.review_id)
     .then((result) => res.status(204).send(result.data))
-    .catch((err) => console.log(err.message));
+    .catch((err) => console.log(`Error: ${err.message}`));
 };
 
 module.exports.getProductInfo = (req, res) => {
   handlers.getProductInfo(req.params.product_id)
-    .then((result) => {
-      // console.log(result.data);
-      res.send(result.data);
-    })
-    .catch((err) => console.log(err.message));
+    .then((result) => res.send(result.data))
+    .catch((err) => console.log(`Error: ${err.message}`));
 };
 
 module.exports.getProductStyles = (req, res) => {
   handlers.getProductStyles(req.params.product_id)
-    .then((result) => {
-      // console.log(result.data);
-      res.send(result.data);
-    })
-    .catch((err) => console.log(err.message));
+    .then((result) => res.send(result.data))
+    .catch((err) => console.log(`Error: ${err.message}`));
 };
