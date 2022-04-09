2007e026b298f8ebd655d13d65d201b6d73ec03a
Zynh Ludwig <Zynh0722@gmail.com>
2022-04-09 07:47:46 -0700

    reviews: add spacing on breakdown bars

--- client/src/components/reviews/rating-breakdown/breakdown-list/BreakdownBar.jsx
+++ client/src/components/reviews/rating-breakdown/breakdown-list/BreakdownBar.jsx
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
+  align-items: center;
 `;
 
 const Text = styled.div`
@@ -18,7 +19,7 @@ const BarContainer = styled.div`
   display: flex;
   flex-grow: 1;
   height: 1em;
-  margin: 0.1em 0 0.1em 0;
+  margin: 0.5em 0 0.5em 0;
   border-radius: 10px;
   overflow: hidden;
   grid-column: 2;
