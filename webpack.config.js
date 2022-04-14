fc1cc55b3ecbac079f3faa2e5ab0ba0437b0dd2b
Zynh Ludwig <Zynh0722@gmail.com>
2022-04-14 15:00:41 -0700

    optimizations: adjusting build scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = (env, argv) => ({
-  mode: 'production',
+  mode: 'development',
   entry: path.join(__dirname, '/client/src/index.jsx'),
   output: {
     path: path.join(__dirname, '/client/dist'),
